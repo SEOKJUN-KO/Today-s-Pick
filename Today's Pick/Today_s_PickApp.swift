257fe1aca9ca86e52632b1ee8a0ff5c75f6f37b6
Seokjun Ko <tjrwns1021@gmail.com>
2023-06-17 15:20:47 +0900

    [INIT] Project
    Today's Pick Project is made

---
+++ Today's Pick/Today_s_PickApp.swift
@@ -0,0 +1,17 @@
+//
+//  Today_s_PickApp.swift
+//  Today's Pick
+//
+//  Created by Ko Seokjun on 2023/06/03.
+//
+
+import SwiftUI
+
+@main
+struct Today_s_PickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
